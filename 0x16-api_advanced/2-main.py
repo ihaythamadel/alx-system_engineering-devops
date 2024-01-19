87132e6491a5aafeadb25617950929d151512051
ihaythamadel <123040960+ihaythamadel@users.noreply.github.com>
2024-01-19 02:18:05 +0200

    Update 2-main.py

--- 0x16-api_advanced/2-main.py
+++ 0x16-api_advanced/2-main.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
 """
-Using reddit's API
+2-main
 """
-import requests
-after = None
+import sys
 
-
-def recurse(subreddit, hot_list=[]):
-    """returning the top ten post titles in a recursively"""
-    global after
-    user_agent = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
-
-    if results.status_code == 200:
-        after_data = results.json().get("data").get("after")
-        if after_data is not None:
-            after = after_data
-            recurse(subreddit, hot_list)
-        all_titles = results.json().get("data").get("children")
-        for title_ in all_titles:
-            hot_list.append(title_.get("data").get("title"))
-        return hot_list
+if __name__ == '__main__':
+    recurse = __import__('2-recurse').recurse
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        return (None)
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
